72d04e28ddf15f71ad698fc43cc149ea80456dfa
Sashit Sharma <sashitsharma@gmail.com>
2023-07-27 16:45:14 +0200

    initial commit

--- intech_calculator.py
+++ intech_calculator.py
@@ -1,132 +1,188 @@
-################### python code for calculator gui ####################
 import tkinter as tk
+import math
 
-# Create app window
+number1 = number2 = operator = None
+
+def get_digit(digit):
+    current = result_label["text"]
+    new = current + str(digit)
+    result_label.config(text=new)
+
+
+def clear():
+    result_label.config(text=" ")
+    equation_label.config(text=" ")
+
+
+def get_operator(op):
+    global number1, operator
+    number1 = int(result_label["text"])
+    operator = op
+    result_label.config(text="")
+    equation_label.config(text=f"{number1} {operator}")
+
+
+def calculate_sin():
+    try:
+        global number1, operator
+        number1 = int(result_label["text"])
+        angle_radians = math.radians(number1)
+        result = math.sin(angle_radians)
+        result_label.config(text=f"Sine: {result:.4f}")
+        equation_label.config(text=f"sin({number1}) =")
+    except ValueError:
+        result_label.config(text="Invalid input")
+
+
+def calculate_cos():
+    try:
+        global number1, operator
+        number1 = int(result_label["text"])
+        angle_radians = math.radians(number1)
+        result = math.cos(angle_radians)
+        result_label.config(text=f"Cosine: {result:.4f}")
+        equation_label.config(text=f"cos({number1}) =")
+    except ValueError:
+        result_label.config(text="Invalid input")
+
+
+def get_result():
+    global number1, number2, operator
+
+    if operator == " √ ":
+        try:
+            root_square = round(math.sqrt(number1), 5)
+            result_label.config(text=str(root_square))
+            equation_label.config(text=f"√({number1}) =")
+        except ValueError:
+            result_label.config(text="Error")
+            equation_label.config(text="")
+
+    elif operator == "+":
+        number2 = int(result_label["text"])
+        result_label.config(text=str(number1 + number2))
+        equation_label.config(text=f"{number1} + {number2} =")
+
+    elif operator == "-":
+        number2 = int(result_label["text"])
+        result_label.config(text=str(number1 - number2))
+        equation_label.config(text=f"{number1} - {number2} =")
+
+    elif operator == "*":
+        number2 = int(result_label["text"])
+        result_label.config(text=str(number1 * number2))
+        equation_label.config(text=f"{number1} * {number2} =")
+
+    elif operator == "x^y":
+        number2 = int(result_label["text"])
+        result_label.config(text=str(number1 ** number2))
+        equation_label.config(text=f"{number1} ^ {number2} =")
+
+    else:
+        number2 = int(result_label["text"])
+        if number2 == 0:
+            result_label.config(text="Error")
+            equation_label.config(text="")
+        else:
+            result_label.config(text=str(round(number1 / number2, 5)))
+            equation_label.config(text=f"{number1} / {number2} =")
+
+
 win = tk.Tk()
-
-# Specify screen size of the Application window
-win.geometry("312x324")
-
-# Disable resizing the app window
+win.geometry("300x485")
 win.resizable(0, 0)
-
-# Specify application name
 win.title("in-tech Calculator")
+win.configure(background="teal")
 
-# ------- Write required functions tkinter-calculator ----------
+border_frame = tk.Frame(win, borderwidth=2, relief=tk.GROOVE, background="maroon")
+border_frame.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
 
-input_value = ""
+result_label = tk.Label(border_frame, text="", bg="maroon", fg="white")
+result_label.grid(row=1, column=0, columnspan=10, pady=(50, 25), sticky="w")
+result_label.config(font=("verdana", 20, "bold"))
 
-# Intialize StringVar
-display_text = tk.StringVar()
+equation_label = tk.Label(border_frame, text="", bg="maroon", fg="white")
+equation_label.grid(row=0, column=0, columnspan=10, pady=(0, 5), sticky="w")
+equation_label.config(font=("Arial", 12))
 
+btn_7 = tk.Button(border_frame, text="7", fg="Black", bg="Skyblue", width=5, height=2, cursor="hand2", command=lambda: get_digit(7))
+btn_7.grid(row=2, column=0)
+btn_7.config(font=("verdana", 14))
 
-# Function to continuously updates input field whenever you click a button
-def click_button_action(item):
-    global input_value
-    input_value = input_value + str(item)
-    display_text.set(input_value)
+btn_8 = tk.Button(border_frame, text="8", fg="Black", bg="Skyblue", width=5, height=2, cursor="hand2", command=lambda: get_digit(8))
+btn_8.grid(row=2, column=1)
+btn_8.config(font=("verdana", 14))
 
+btn_9 = tk.Button(border_frame, text="9", fg="Black", bg="Skyblue", width=5, height=2, cursor="hand2", command=lambda: get_digit(9))
+btn_9.grid(row=2, column=2)
+btn_9.config(font=("verdana", 14))
 
-# Function to clear the display
-def clear_button_action():
-    global input_value
-    input_value = ""
-    display_text.set("")
+btn_add = tk.Button(border_frame, text="+", fg="Black", bg="lightgreen", width=5, height=2, cursor="hand2", command=lambda: get_operator("+"))
+btn_add.grid(row=2, column=3)
+btn_add.config(font=("verdana", 14))
 
+btn_4 = tk.Button(border_frame, text="4", fg="Black", bg="Skyblue", width=5, height=2, cursor="hand2", command=lambda: get_digit(4))
+btn_4.grid(row=3, column=0)
+btn_4.config(font=("verdana", 14))
 
-# Function to calculate input values
-def equal_button_action():
-    global input_value
-    result = str(eval(input_value))
-    display_text.set(result)
-    input_value = ""
+btn_5 = tk.Button(border_frame, text="5", fg="Black", bg="Skyblue", width=5, height=2, cursor="hand2", command=lambda: get_digit(5))
+btn_5.grid(row=3, column=1)
+btn_5.config(font=("verdana", 14))
 
+btn_6 = tk.Button(border_frame, text="6", fg="Black", bg="Skyblue", width=5, height=2, cursor="hand2", command=lambda: get_digit(6))
+btn_6.grid(row=3, column=2)
+btn_6.config(font=("verdana", 14))
 
-# ----------------------------------------------------
-# Create a frame for the display field
+btn_minus = tk.Button(border_frame, text="-", fg="Black", bg="lightGreen", width=5, height=2, cursor="hand2", command=lambda: get_operator("-"))
+btn_minus.grid(row=3, column=3)
+btn_minus.config(font=("verdana", 14))
 
-input_frame = tk.Frame(win, width=312, height=50, bd=0, highlightbackground="black", highlightcolor="black",
-                       highlightthickness=2)
+btn_1 = tk.Button(border_frame, text="1", fg="Black", bg="Skyblue", width=5, height=2, cursor="hand2", command=lambda: get_digit(1))
+btn_1.grid(row=4, column=0)
+btn_1.config(font=("verdana", 14))
 
-input_frame.pack(side=tk.TOP)
+btn_2 = tk.Button(border_frame, text="2", fg="Black", bg="Skyblue", width=5, height=2, cursor="hand2", command=lambda: get_digit(2))
+btn_2.grid(row=4, column=1)
+btn_2.config(font=("verdana", 14))
 
-# Create a input field (Entry widget) inside the 'Frame'
+btn_3 = tk.Button(border_frame, text="3", fg="Black", bg="Skyblue", width=5, height=2, cursor="hand2", command=lambda: get_digit(3))
+btn_3.grid(row=4, column=2)
+btn_3.config(font=("verdana", 14))
 
-input_field = tk.Entry(input_frame, font=('arial', 18, 'bold'), textvariable=display_text, width=50, bg="#eee", bd=0,
-                       justify=tk.RIGHT)
+btn_multiply = tk.Button(border_frame, text="*", fg="Black", bg="lightGreen", width=5, height=2, cursor="hand2", command=lambda: get_operator("*"))
+btn_multiply.grid(row=4, column=3)
+btn_multiply.config(font=("verdana", 14))
 
-input_field.grid(row=0, column=0)
+btn_clear = tk.Button(border_frame, text="C", fg="Black", bg="crimson", width=5, height=2, cursor="hand2", command=lambda: clear())
+btn_clear.grid(row=6, column=0, columnspan=1)
+btn_clear.config(font=("verdana", 14))
 
-input_field.pack(ipady=10)  # 'ipady'  is for internal padding to change the height of the input field
+btn_0 = tk.Button(border_frame, text="0", fg="Black", bg="Skyblue", width=5, height=2, cursor="hand2", command=lambda: get_digit(0))
+btn_0.grid(row=5, column=1)
+btn_0.config(font=("verdana", 14))
 
-# -------------------------------------------------------
-# Create another 'Frame' for buttons
+btn_equal = tk.Button(border_frame, text="=", fg="Black", bg="violet", width=5, height=2, cursor="hand2", command=get_result)
+btn_equal.grid(row=6, column=3, columnspan=1)
+btn_equal.config(font=("verdana", 14))
 
-btns_frame = tk.Frame(win, width=312, height=272.5, bg="grey")
+btn_divide = tk.Button(border_frame, text="/", fg="Black", bg="lightGreen", width=5, height=2, cursor="hand2", command=lambda: get_operator("/"))
+btn_divide.grid(row=5, column=3)
+btn_divide.config(font=("verdana", 14))
 
-btns_frame.pack()
+btn_root = tk.Button(border_frame, text=" √ ", fg="Black", bg="lightblue", width=5, height=2, cursor="hand2", command=lambda: get_operator(" √ "))
+btn_root.grid(row=5, column=0, columnspan=1, padx=1, pady=1)
+btn_root.config(font=("verdana", 14))
 
-# 1st row
+btn_exponent = tk.Button(border_frame, text="x^y", fg="Black", bg="lightblue", width=5, height=2, cursor="hand2", command=lambda: get_operator("x^y"))
+btn_exponent.grid(row=5, column=2, columnspan=1, padx=1, pady=1)
+btn_exponent.config(font=("verdana", 14))
 
-clear_btn = tk.Button(btns_frame, text="ALL CLEAR", fg="white", width=32, height=3, bd=0, bg="Gray", cursor="hand2",
-                      command=lambda: clear_button_action()).grid(row=0, column=0, columnspan=3, padx=1, pady=1)
+btn_sin = tk.Button(border_frame, text="sin", fg="Black", bg="lightgray", width=5, height=2, cursor="hand2", command=calculate_sin)
+btn_sin.grid(row=6, column=1, columnspan=1, padx=1, pady=1)
+btn_sin.config(font=("verdana", 14))
 
-divide_btn = tk.Button(btns_frame, text="/", fg="black", width=10, height=3, bd=0, bg="orange", cursor="hand2",
-                       command=lambda: click_button_action("/")).grid(row=0, column=3, padx=1, pady=1)
+btn_cos = tk.Button(border_frame, text="cos", fg="Black", bg="lightgray", width=5, height=2, cursor="hand2", command=calculate_cos)
+btn_cos.grid(row=6, column=2, columnspan=1, padx=1, pady=1)
+btn_cos.config(font=("verdana", 14))
 
-# 2nd row
-
-btn_7 = tk.Button(btns_frame, text="7", fg="black", width=10, height=3, bd=0, bg="skyblue", cursor="hand2",
-                  command=lambda: click_button_action(7)).grid(row=1, column=0, padx=1, pady=1)
-
-btn_8 = tk.Button(btns_frame, text="8", fg="black", width=10, height=3, bd=0, bg="skyblue", cursor="hand2",
-                  command=lambda: click_button_action(8)).grid(row=1, column=1, padx=1, pady=1)
-
-btn_9 = tk.Button(btns_frame, text="9", fg="black", width=10, height=3, bd=0, bg="skyblue", cursor="hand2",
-                  command=lambda: click_button_action(9)).grid(row=1, column=2, padx=1, pady=1)
-
-multiply_btn = tk.Button(btns_frame, text="X", fg="black", width=10, height=3, bd=0, bg="orange", cursor="hand2",
-                         command=lambda: click_button_action("*")).grid(row=1, column=3, padx=1, pady=1)
-
-# 3rd row
-
-btn_4 = tk.Button(btns_frame, text="4", fg="black", width=10, height=3, bd=0, bg="skyblue", cursor="hand2",
-                  command=lambda: click_button_action(4)).grid(row=2, column=0, padx=1, pady=1)
-
-btn_5 = tk.Button(btns_frame, text="5", fg="black", width=10, height=3, bd=0, bg="skyblue", cursor="hand2",
-                  command=lambda: click_button_action(5)).grid(row=2, column=1, padx=1, pady=1)
-
-btn_6 = tk.Button(btns_frame, text="6", fg="black", width=10, height=3, bd=0, bg="skyblue", cursor="hand2",
-                  command=lambda: click_button_action(6)).grid(row=2, column=2, padx=1, pady=1)
-
-minus_btn = tk.Button(btns_frame, text="-", fg="black", width=10, height=3, bd=0, bg="orange", cursor="hand2",
-                      command=lambda: click_button_action("-")).grid(row=2, column=3, padx=1, pady=1)
-
-# 4th row
-
-btn_1 = tk.Button(btns_frame, text="1", fg="black", width=10, height=3, bd=0, bg="skyblue", cursor="hand2",
-                  command=lambda: click_button_action(1)).grid(row=3, column=0, padx=1, pady=1)
-
-btn_2 = tk.Button(btns_frame, text="2", fg="black", width=10, height=3, bd=0, bg="skyblue", cursor="hand2",
-                  command=lambda: click_button_action(2)).grid(row=3, column=1, padx=1, pady=1)
-
-btn_3 = tk.Button(btns_frame, text="3", fg="black", width=10, height=3, bd=0, bg="skyblue", cursor="hand2",
-                  command=lambda: click_button_action(3)).grid(row=3, column=2, padx=1, pady=1)
-
-plus_btn = tk.Button(btns_frame, text="+", fg="black", width=10, height=3, bd=0, bg="orange", cursor="hand2",
-                     command=lambda: click_button_action("+")).grid(row=3, column=3, padx=1, pady=1)
-
-# 5th row
-
-btn_0 = tk.Button(btns_frame, text="0", fg="black", width=21, height=3, bd=0, bg="skyblue", cursor="hand2",
-                  command=lambda: click_button_action(0)).grid(row=4, column=0, columnspan=2, padx=1, pady=1)
-
-point_btn = tk.Button(btns_frame, text=".", fg="black", width=10, height=3, bd=0, bg="skyblue", cursor="hand2",
-                      command=lambda: click_button_action(".")).grid(row=4, column=2, padx=1, pady=1)
-
-equals_btn = tk.Button(btns_frame, text="=", fg="black", width=10, height=3, bd=0, bg="orange", cursor="hand2",
-                       command=lambda: equal_button_action()).grid(row=4, column=3, padx=1, pady=1)
-
-# Run main loop
-win.mainloop()
+border_frame.mainloop()
